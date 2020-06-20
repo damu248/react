46688698da012c1a6c93a442f173f920bd5c409e
damu248 <67195945+damu248@users.noreply.github.com>
2020-06-20 20:14:16 +0530

    Add files via upload

---
+++ card-list.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './card-list.style.css'
+
+export const CardList = props  => (
+    <div className='card-list'>
+    {
+        props.Monsters.map(monster => 
+        (<h1 key={monster.id}>{monster.name}</h1>
+        ))}
+
+    </div>
+)
